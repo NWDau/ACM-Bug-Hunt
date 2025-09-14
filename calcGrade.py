3edd4781742c6a361d444ba2fa28067c869b1c34
Addison Rinehart <S557217@nwmissouri.edu>
2025-09-14 17:37:52 -0500

    Create calcGrade.py

---
+++ calcGrade.py
@@ -0,0 +1,23 @@
+### Grade Calculator
+
+#functions
+def grade (avg):
+  if (avg < 60): letter = 'F'
+  elif (avg < 70): letter = 'D'
+  elif (avg < 80): letter = 'C'
+  elif (avg < 90): letter = 'B'
+  else: letter = 'A'
+  return letter
+
+def calcAvg (exam1, exam2, exam3, exam4):
+  avg = (exam1 + exam2 + exam3 + exam4)/4
+  return avg
+
+#MAIN
+##input
+exams = [0]*4
+for i in range(4):
+  exams[i] = float(input("Enter exam score: "))
+##calc average score & grade
+avg = calcAvg(exams[1], exams[2], exams[3], exams[4])
+print("Average:", avg, "\nGrade:", grade(avg))
