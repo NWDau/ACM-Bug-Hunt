9d5c49bd572308c7055363e12980007b53013447
Addison Rinehart <S557217@nwmissouri.edu>
2025-09-14 20:07:32 -0500

    Create addNum.py

---
+++ addNum.py
@@ -0,0 +1,13 @@
+# add some numbers. what could possibly go wrong?
+total = 0
+for i in range(5):
+    while True:
+        try:
+            temp = int(input("Enter an interger: "))
+            break
+        except ValueError:
+            print("Invalid value. Please try again.")
+            continue
+        total += temp
+
+print("Total: ", total)
